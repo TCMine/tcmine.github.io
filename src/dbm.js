3f9c3ac7395e11726acb3805a6d2a76d9dbcc386
TCMine <32180878+TCMine@users.noreply.github.com>
2023-02-21 03:45:41 -0600

    stuff

--- src/dbm.js
+++ src/dbm.js
@@ -15,8 +15,10 @@ function isJsonString(str) {
 }
 
 function joe() {
+    document.getElementById("invalid").style.display="none";
+    clear()
     var val = ""
-    console.log(RDInput.value);
+    //console.log(RDInput.value);
     if (RDInput.value === '') return document.getElementById("invalid").style.animation="hideInvalid 0.3s linear forwards";
     if (isJsonString(RDInput.value)) {
         //document.getElementById("invalid").style.display = "none";
@@ -26,11 +28,51 @@ function joe() {
         loadData(val)
 
     } else {
+        document.getElementById("invalid").style.display="grid";
         document.getElementById("invalid").style.animation="showInvalid 0.3s linear forwards";
     }
-    console.log(val)
+    //console.log(val)
 }
 
-function loadData() {
+var rd_actionlist;
+var rd_info;
+
+function loadData(val) {
+
+    if (rd_actionlist) {
+        rd_actionlist.remove()
+    }
+
+    let data = JSON.parse(val);
+    let joe = document.createElement("form")
+    let container = document.createElement("div")
+    container.className = "action-list"
+    joe.className = "container"
+    joe.id = "actionlist"
+    //console.log(data.actions)
+    var actions = data.actions
+    joe.appendChild(container);
     
+    actions.forEach((item, index) => {
+        console.log(item.name)
+        const button = document.createElement("div");
+        button.innerHTML = `<b>#${index+1}</b> ${item.name}`; // Use the name property of each object as the button text
+        button.className = "action-item"; // Use the name property of each object as the button text
+        container.appendChild(button); // Add the button to the buttonListDiv
+    });
+    
+    document.body.appendChild(joe)
+    rd_actionlist = joe
+    joe.scrollIntoView({ behavior: 'smooth', block: 'center'});
+}
+
+function clear() {
+    try {
+        rd_actionlist.remove()
+    } catch (e) {
+    }
+    try {
+        rd_info.remove()
+    } catch (e) {
+    }
 }
